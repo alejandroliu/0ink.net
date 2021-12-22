417661360d7eb068145af8195e0988f4581f513a
Alejandro Liu <alejandro_liu@hotmail.com>
2021-12-22 14:03:35 +0100

    MVP2: added mdcat to extras, re-org posts, tweaked template

---
+++ src/content/posts/2021/2021-12-21-linux-tips.md
@@ -0,0 +1,37 @@
+---
+title: Linux Post Install tasks
+date: 2021-12-21
+---
+
+These tips are for [void linux][void] as that is the distro
+I am using nowadays.
+
+- mate tricks: change background from cli
+    - `dconf write /org/mate/desktop/background/picture-filename "'PATH-TO-JPEG'"`
+- web page to check if your browser is html5 compliant:
+    - [https://www.youtube.com/html5](https://www.youtube.com/html5)
+
+# HotKeys
+
+- Install `xbindkeys`
+- Add to startup? `$HOME/.xprofile`
+- Create default config with `xbindkeys -d > $HOME/.xbindkeysrc`
+- Lookup key combinations:
+    - `xbindkeys --multikey` or
+    - `xbindkeys --key`
+- update bindings `xbindkeys --poll-rc`
+- `rofi`
+    - A good addition to this is `rofi`.  Which is a dynamic menu.
+    - Create a xbindkey shortcut to run rofi with:
+	- `rofi -show-icons  -modi drun,window  -show drun`
+
+# Additional software
+
+- Additional software (Tested on void linux):
+  - Tip: use `sed -e 's/#.*$//'` to strip comments!
+
+<script src="https://tortugalabs.github.io/embed-like-gist/embed.js?style=paraiso-light&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on&fetchFromJsDelivr=on&target=https://github.com/alejandroliu/0ink.net/blob/master/snippets/void-installation/swlist-extras.txt"></script>
+
+
+
+ [void]: https://voidlinux.org "Void Linux"
--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -3,6 +3,7 @@
 
 AUTHOR = 'Alejandro Liu'
 SITENAME = '0ink.net'
+SITESUBTITLE = 'Not the site you are looking for...'
 SITEURL = ''
 
 PATH = 'content'
@@ -20,15 +21,18 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-          ('Familia','https://www.iliu.net'),
+          ('Repo','https://github.com/alejandroliu/0ink.net'),
+          ('Alejandro','https://alejandro.iliu.net/'),
+          #('Tags','/tags.html'),
         )
+LOGO_IMG = '/images/2021/0ink.png'
+DISPLAY_PAGES_ON_MENU = True
 
 # Social widget
 SOCIAL = (
-          ('github','https://github.com/alejandroliu/0ink.net'),
+          ('github','https://github.com/alejandroliu/'),
+          ('LinkedIn','https://www.linkedin.com/in/alejandro-liu-ly/'),
          )
-
-DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -42,4 +46,21 @@ EXTRA_PATH_METADATA = {
     'extras/CNAME': {'path': 'CNAME'},
 }
 
+SLUGIFY_SOURCE = 'basename'
+ARTICLE_URL = 'posts/{date:%Y}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{slug}.html'
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+
+# Pagination
+DEFAULT_PAGINATION = 10
+
+THEME = 'themes/pelican-simplegrey'
+
+# ~ THEME = 'pelican-themes/gum' # styling is a little off
+# ~ THEME = 'pelican-themes/Just-Read' # nav is wacked
+# ~ THEME = 'pelican-themes/pelican-sober' # Maybe
+
+SEARCH_SITE="0ink.net"
+SEARCH_PREFILL="Search with DuckDuckGo"
 
