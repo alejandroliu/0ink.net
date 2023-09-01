ec06b673ba10b53c971f7f16fd21ec686bf72f45
Alejandro Liu <alejandro_liu@hotmail.com>
2023-09-01 21:59:08 +0200

    Added options to zzdm

--- snippets/void-installation/noxdm/zzdm.sh
+++ snippets/void-installation/noxdm/zzdm.sh
@@ -3,5 +3,8 @@ if [[ -z "$DISPLAY" && $(tty) == /dev/tty1 && $(id -u) -gt 100 ]]; then
   export XINITRC=/etc/X11/Xsession
   . $HOME/.bashrc # Make sure PATH is set-up
   clear
-  exec startx
+  XCLIENT_OPTS=""
+  XSERVER_OPTS=""
+  [ -f /etc/X11/zzdm-opts.sh ] && . /etc/X11/zzdm-opts.sh
+  exec startx $XCLIENT_OPTS -- $XSERVER_OPTS
 fi
